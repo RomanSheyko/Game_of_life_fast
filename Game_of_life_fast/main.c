cec038566352daa88924eccb0d163c1eece08461
Roman <rsheyko1997@gmail.com>
2018-12-26 03:18:36 +0300

    Initial Commit

---
+++ Game_of_life_fast/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Game_of_life_fast
+//
+//  Created by Роман on 26/12/2018.
+//  Copyright © 2018 Roman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
